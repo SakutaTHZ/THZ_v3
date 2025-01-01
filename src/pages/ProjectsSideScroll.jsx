ab85334eb0c5e361787b46ed1fef0436ed431581
Thar Htet Zan <110976118+SakutaTHZ@users.noreply.github.com>
2025-01-01 15:39:14 +0630

    So many bugs to fix

--- src/pages/ProjectsSideScroll.jsx
+++ src/pages/ProjectsSideScroll.jsx
@@ -1,9 +1,73 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Lenis from "@studio-freight/lenis";
 
 const ProjectSideScroll = () => {
-  document.addEventListener("DOMContentLoaded", () => {
-    gsap.registerPlugin(ScrollTrigger)
-  });
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const lenis = new Lenis();
+    lenis.on("scroll", () => {
+      ScrollTrigger.update();
+    });
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 1000);
+    });
+    gsap.ticker.lagSmoothing(0);
+
+    const stickySection = document.querySelector(".steps");
+    const stickyHeight = stickySection.scrollHeight + window.innerHeight * 3;
+    const cards = document.querySelectorAll(".card");
+    const countContainer = document.querySelector(".card-container");
+    const totalCards = cards.length;
+
+    // ScrollTrigger.create({
+    //   trigger: stickySection,
+    //   start: "top top",
+    //   end: `+=${stickyHeight}px`,
+    //   pin: true,
+    //   pinSpacing: true,
+    //   onUpdate: (self) => {
+    //     positionCards(self.progress);
+    //   },
+    // });
+
+    // const getRadius = () => {
+    //   return window.innerHeight < 900
+    //     ? window.innerWidth * 7.5
+    //     : window.innerHeight * 2.5;
+    // };
+    // const arcAngle = Math.PI * 0.4;
+    // const startAngle = Math.PI / 2 - arcAngle / 2;
+
+    // const positionCards = (progress = 0) => {
+    //   const radius = getRadius();
+    //   const totalTravel = 1 + totalCards / 7.5;
+    //   const adjustedPorgress = (progress * totalTravel - 1) * 0.75;
+
+    //   cards.forEach((card, i) => {
+    //     const normalizedProgress = (totalCards - 1 - i) / totalCards;
+    //     const cardProgress = normalizedProgress + adjustedPorgress;
+    //     const angle = startAngle + arcAngle + cardProgress;
+
+    //     const x = Math.cos(angle) * radius;
+    //     const y = Math.sin(angle) * radius;
+    //     const rotation = (angle - Math.PI / 2) * (180 / Math.PI);
+
+    //     gsap.set(card, {
+    //       x: x,
+    //       y: -y + radius,
+    //       rotation: -rotation,
+    //       transformOrigin: "center center",
+    //     });
+    //   });
+    // };
+
+    // positionCards(0)
+
+  }, []);
+
   return (
     <>
       <div className="projectSideScrollSection">
